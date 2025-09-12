9cfbcd0c7b9ff1bac2a5d24945ce069329115acd
Soman Abbasi <mssabbasi306@gmail.com>
2025-09-13 00:47:24 +0500

    resume

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+     <div className="p-10 text-center">
+      <h1 className="text-4xl font-bold text-blue-600">Tailwind with Vite Plugin</h1>
+      <p className="text-gray-600 mt-2">Setup via @tailwindcss/vite</p>
+    </div>
+      
+  )
+}
+
+export default App
